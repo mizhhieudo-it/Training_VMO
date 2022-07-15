7e33101b4d8641b9ae2d793e598e705fd0e38e31
mizhhieudo <minhhieudo.it@gmail.com>
2022-07-15 14:26:17 +0700

    update-first-app-nodejs

--- Day4-TrainingNodeJS/nodeJS01_firstNodeJSProgram/src/app.js
+++ Day4-TrainingNodeJS/nodeJS01_firstNodeJSProgram/src/app.js
@@ -1,12 +1,10 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// import express, { Request, Response } from "./node_modules/express";
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.get("/", (req, res) => {
-    res.status(200).send("Welcome to NodeJS");
-});
-app.listen(1234);
+const http = require("http");
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html; charset=UTF-8')
+    res.write('<h3>welcome to NODEJS </h3>')
+    res.end()
+})
+server.listen(1234, "localhost", () => {
+    console.log("Server runing port 1234 ...")
+})

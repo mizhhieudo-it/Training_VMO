f920da94570ab7b9b7d8cab9d370646528837c9d
HieuMizhDoDev <minhhieudo.it@gmail.com>
2022-08-08 11:57:28 +0700

    update-bussines-project-to-type-project

--- ProjectEndCourse/project-demo/app.module.ts
+++ ProjectEndCourse/project-demo/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { testModule } from 'Apis/V1/testAPI/test.module';
 import { UserModule } from 'Apis/V1/user/user.module';
 import { AuthModule } from 'Shared/Auth/auth.module';
-import { ProjectModule } from 'Apis/V1/project/project.module';
+import { ProjectModule } from 'Apis/V1/projectTypes/projectTypes.module';
 import { StatusProjectModule } from 'Apis/V1/statusProject/stautsProject.module';
 import { technologyModule } from 'Apis/V1/technology/technology.module';
 import { employeeModule } from 'Apis/V1/employee/employee.module';
